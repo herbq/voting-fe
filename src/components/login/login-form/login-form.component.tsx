64cc7efcde82fb28c4f63cd32574650ae5997f66
herbq <81512633+stqven@users.noreply.github.com>
2023-03-24 22:40:30 +0200

    adding b

--- src/components/login/login-form/login-form.component.tsx
+++ src/components/login/login-form/login-form.component.tsx
@@ -11,13 +11,13 @@ import useLogin from "../../../hooks/login.hook";
 
 const LoginForm = () => {
   const { inputs, others } = useLogin();
-  console.log(`env`, process.env.REACT_APP_BACKEND);
+  console.log(`env`, process.env.REACT_APP_BACKEND);F
 
   return (
     <div className="form-container">
       <div className="log-in-container">
         <form id="login" className="form" onSubmit={others.submitHandler}>
-          <h1>WELCOMEe</h1>
+          <h1>WELCOMEeb</h1>
           {others.isRegisterLayout && <Input name="email" placeholder="Email" value={inputs.email.value} onChange={inputs.email.set} isValid={inputs.email.isValid}></Input>}
           <Input id="natid" name="natid" placeholder="National ID" value={inputs.identification.value} onChange={inputs.identification.set} isValid={!others.isRegisterLayout || inputs.identification.value}></Input>
           <Input id="passwd" name="passwd" placeholder="Password" value={inputs.password.value} onChange={inputs.password.set} isValid={!others.isRegisterLayout || inputs.password.isValid} type="password">
